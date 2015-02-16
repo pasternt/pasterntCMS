639e54829df8369dd56a69d26daf13ac5d02aa89
pasternt <pasterntt@gmail.com>
2015-02-16 21:47:38 +0100

    Downloads aufgeräj

--- install/rmdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once "../config.php";
-//mysql_query("DROP TABLE IF EXISTS `user`;");
-//mysql_query("DROP TABLE IF EXISTS `content`;");
-//mysql_query("DROP TABLE IF EXISTS `navbar`;");
-mysql_query("UPDATE user WHERE name=`admin` SET banned=1");
-
-?>
